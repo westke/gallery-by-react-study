e193dd1130af84cb698d4c9c7fc993faf4e382d0
wester <wangchuanyin2010@163.com>
2016-09-08 19:21:31 +0800

    修改scss配置
    修改scss配置，修改图片，增加页面逻辑和样式

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,83 @@ require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+var imageDatas = require('../datas/imageData.json');
 
-class AppComponent extends React.Component {
+//  利用自执行函数，将图片名信息转成图片URL路径信息
+var imageDatas = require('../datas/imageData.json');
+imageDatas =  (function genImageURL(imageDataArr) {
+  for (var i = 0, j = imageDataArr.length; i < j; i++) {
+    var singleImageData = imageDataArr[i];
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+    imageDataArr[i] = singleImageData;
+  }
+  return imageDataArr
+})(imageDatas);
+
+class ImgFigure extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL}/>
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
+class AppComponent extends React.Component {
+  Constant : {
+    centerPos: {
+      left: 0,
+      right: 0
+    },
+    hPosRange: {     //水平方向的取值范围
+      leftSecX: [0, 0],
+      rightSecX: [0, 0],
+      y: [0, 0]
+    },
+    vPosRange: {     //垂直方向的取值范围
+      x: [0, 0],
+      topY: [0, 0]
+    }
+  }
+
+  componentDidMount() {
+    //首先获取舞台的大小
+    var stageDOM = React.findDOMNode(this.ref.stage),
+        stageW = stageDOM.scrollWidth,
+        stageH = stageDOM.scrollHeight,
+        halfStageW = Math.ceil(stageW / 2),
+        halfStageH = Math.ceil(stageH / 2);
+
+    //拿到一个imageFigure的大小
+    var imageFigureDOM = React.findDOMNode(this.ref.imgFigure0),
+        imgW = imageFigureDOM.scrollWidth,
+        imgH = imageFigureDOM.scrollHeight,
+        halfImgW = Math.ceil(imgW / 2),
+        halfImgH = Math.ceil(imgH / 2)
+  }
+
+  render() {
+
+    var controllerUnits = [],
+      imgFigures = [];
+
+    imageDatas.forEach(function (value, index) {
+      imgFigures.push(<ImgFigure data={value} ref={'imgFigure'+index}/>)
+    })
+
+    return (
+      <section className="stage" ref="stage">
+        <section className="img-sec">
+          {imgFigures}
+        </section>
+        <nav className="controller-nav">
+          {controllerUnits}
+        </nav>
+      </section>
     );
   }
 }
